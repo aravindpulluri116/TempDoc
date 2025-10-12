1ad5f6af89e213538a7c7d821ad0d36bf6a9de0f
ARAVIND PULLURI <pulluriaravind@gmail.com>
2025-10-12 16:19:03 +0000

    see

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -15,8 +15,8 @@ export function ThemeToggle() {
       className="rounded-full hover:bg-accent/80 focus-visible:ring-accent"
       aria-label="Toggle theme"
     >
-      <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-primary-foreground" />
-      <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-primary-foreground" />
+      <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-foreground" />
+      <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-foreground" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
